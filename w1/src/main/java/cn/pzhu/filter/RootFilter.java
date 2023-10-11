aa5b8b2b6f9c31ee1fa76e17a3d22f250df30222
lanquanxiang <lanquanxiang@gmail.com>
2023-10-11 11:50:19 +0800

    update

---
+++ w1/src/main/java/cn/pzhu/filter/RootFilter.java
@@ -0,0 +1,37 @@
+package cn.pzhu.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class F1Filter
+ */
+@WebFilter("/userinfo.jsp")
+public class RootFilter extends HttpFilter {
+       
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		if(req.getSession().getAttribute("user")==null) {
+			//res.sendRedirect("login.jsp");
+			res.setContentType("text/html");
+			res.getWriter().print("<script>alert('你还没有登录，请先登录...');location.href='login.jsp';</script>");
+		}else {
+			super.doFilter(req, res, chain);
+		}
+	}
+
+	
+
+}
---
+++ w1/src/main/java/cn/pzhu/listener/OnlineListener.java
@@ -0,0 +1,63 @@
+package cn.pzhu.listener;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+import cn.pzhu.pojo.User;
+
+/**
+ * Application Lifecycle Listener implementation class OnlineListener
+ *
+ */
+@WebListener
+public class OnlineListener implements HttpSessionAttributeListener {
+	
+	private static int online = 0;
+
+    /**
+     * Default constructor. 
+     */
+    public OnlineListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
+     */
+    public void attributeAdded(HttpSessionBindingEvent se)  { 
+    	synchronized (OnlineListener.class) {
+    		 if(se.getName().equals("user")) {
+            	 online = online +1;
+            	 se.getSession().getServletContext().setAttribute("online", online);
+            	 if (se.getValue() instanceof User user) {
+    				System.out.println(user.getUsername()+"上线了....");
+    			}
+             }
+		}
+        
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeRemoved(HttpSessionBindingEvent)
+     */
+    public void attributeRemoved(HttpSessionBindingEvent se)  { 
+    	synchronized (OnlineListener.class) {
+   		 if(se.getName().equals("user")) {
+           	 online = online - 1;
+           	 se.getSession().getServletContext().setAttribute("online", online);
+           	 if (se.getValue() instanceof User user) {
+   				System.out.println(user.getUsername()+"下线了....");
+   			}
+            }
+		}
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeReplaced(HttpSessionBindingEvent)
+     */
+    public void attributeReplaced(HttpSessionBindingEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+	
+}
