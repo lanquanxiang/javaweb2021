bedec60bbcc41e1e8a393ac9d1f1f62f6c35937d
lanquanxiang <lanquanxiang@gmail.com>
2023-11-02 10:10:23 +0800

    update

---
+++ w2/src/main/java/cn/pzhu/controller/RegistServlet.java
@@ -0,0 +1,86 @@
+package cn.pzhu.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSON;
+
+import cn.pzhu.pojo.Result;
+import cn.pzhu.pojo.User;
+import cn.pzhu.pojo.UserInfo;
+import cn.pzhu.service.UserService;
+import cn.pzhu.service.imp.UserServiceImp;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/regist")
+public class RegistServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	//初始化业务层接口
+	private UserService us = new UserServiceImp();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegistServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String path = request.getContextPath(); //${pageContext.request.ContextPath}  /w2
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");	
+		String email = request.getParameter("email");
+		String sgender = request.getParameter("gender");
+		String [] types = request.getParameterValues("type");
+		String code = request.getParameter("code");
+		if (!"gbcw".equalsIgnoreCase(code)) {
+			request.getSession().setAttribute("msg", "验证码错误");
+			response.sendRedirect("error.jsp");
+			return;
+		}
+		User user = new User(username, password,1);
+		int gender = 1;
+		try {
+			gender = Integer.parseInt(sgender);
+			if(gender!=0 && gender!=1) {
+				throw new Exception();
+			}
+			
+		} catch (Exception e) {
+			request.getSession().setAttribute("msg", "性别数据异常");
+			response.sendRedirect("error.jsp");
+			return;
+			
+		}
+		String type = JSON.toJSONString(types);
+		UserInfo userinfo = new UserInfo(username, email, gender, type);
+    	Result res = us.regist(user,userinfo);//调用业务层接口的方法
+		if(!res.isSuccess()) {
+			request.getSession().setAttribute("msg", res.getMsg());
+			response.sendRedirect(path+"/error.jsp");
+			return;
+		}	
+		response.setContentType("text/html");
+		response.getWriter().print("<script>alert('注册成功，请登录...');location.href='login.jsp';</script>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- w2/src/main/java/cn/pzhu/service/imp/UserServiceImp.java
+++ w2/src/main/java/cn/pzhu/service/imp/UserServiceImp.java
@@ -1,7 +1,11 @@
 package cn.pzhu.service.imp;
 
+import java.util.regex.Pattern;
+
 import cn.pzhu.dao.UserDAO;
+import cn.pzhu.dao.UserInfoDAO;
 import cn.pzhu.dao.imp.UserDAOImp;
+import cn.pzhu.dao.imp.UserInfoDAOImp;
 import cn.pzhu.pojo.Result;
 import cn.pzhu.pojo.User;
 import cn.pzhu.pojo.UserInfo;
@@ -10,6 +14,7 @@ import cn.pzhu.service.UserService;
 public class UserServiceImp implements UserService{
 	//初始化数据访问层接口【考试】
 	private UserDAO userdao = new UserDAOImp();
+    private   UserInfoDAO userinfodao=new UserInfoDAOImp();
 
 	@Override
 	public Result login(User user) {
@@ -37,8 +42,38 @@ public class UserServiceImp implements UserService{
 
 	@Override
 	public Result regist(User user, UserInfo userinfo) {
-		// TODO Auto-generated method stub
-		return null;
+		if(user==null || user.getUsername()==null || user.getPassword()==null) {
+			return new Result(false, "前端数据异常！");
+		}
+		if(user.getUsername().equals("")) {
+			return new Result(false, "用户名不能为空！");
+		}
+		if(user.getPassword().equals("")) {
+			return new Result(false, "密码不能为空！");
+		}
+		if(userinfo==null||userinfo.getEmail()==null) {
+			return new Result(false, "前端数据异常！");
+		
+		}
+		if(userinfo.getEmail().equals("")) {
+			return new Result(false, "邮箱不能为空！");
+		}
+		String reg="^[0-9A-z]{1,30}@[0-9A-z]{1,30}\\.[0-9A-z]$";
+		if(!Pattern.matches(reg, userinfo.getEmail())) {
+			return new Result(false, "邮箱格式不正确！");
+		}
+		if("null".equals(userinfo.getType())) {
+			return new Result(false, "关注类型不能为空！");
+		}
+		if(userdao.selectById(user.getUsername())!=null) {
+			return new Result(false, "此账号已存在！");
+		}
+		if(userdao.insert(user) && userinfodao.insert(userinfo)) {
+			return new Result(true, "注册成功！");
+		}
+		else {
+			return new Result(false, "注册失败！");
+		}
 	}
 
 	@Override
